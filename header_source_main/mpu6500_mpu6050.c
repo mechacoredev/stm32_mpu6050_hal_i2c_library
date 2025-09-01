6a6ad46ecd390e9778bdb2a0fbce578e753c3fca
mechacoredev <eneserbulut67890@gmail.com>
2025-09-01 16:17:55 +0300

    Update mpu6500_mpu6050.c

--- header_source_main/mpu6500_mpu6050.c
+++ header_source_main/mpu6500_mpu6050.c
@@ -1,182 +1,184 @@
 /*
- * mpu6500_mpu6050.c
+ * mpu6050.c
  *
- *  Created on: Aug 11, 2025
+ *  Created on: Sep 1, 2025
  *      Author: Enes
  */
 
-#include "mpu6500_mpu6050.h"
 
-static MPU6xx0_Status _mpu6xx0readregister(MPU6xx0_t *dev, uint16_t MemAddress,
-		uint8_t *pData, uint16_t Size) {
+#include "mpu6050.h"
+#include <stdlib.h>
 
-	if (HAL_I2C_Mem_Read(dev->i2c_handle, dev->i2c_add, MemAddress,
-	I2C_MEMADD_SIZE_8BIT, pData, Size, HAL_MAX_DELAY) == HAL_OK) {
-		return MPU6xx0_OK;
+typedef enum{
+	i2c_addr_0=0xD0,
+	i2c_addr_1=0xD2
+}mpu6050_i2c_addr;
+
+typedef enum{
+	who_am_i_0=0x68,
+	who_am_i_1=0x70
+}mpu6050_who_am_i;
+
+typedef enum {
+	MPU6050_WHO_AM_I     = 0x75,
+
+	MPU6050_XG_OFFSET_H  = 0x13,
+	MPU6050_XG_OFFSET_L  = 0x14,
+	MPU6050_YG_OFFSET_H  = 0x15,
+	MPU6050_YG_OFFSET_L  = 0x16,
+	MPU6050_ZG_OFFSET_H  = 0x17,
+	MPU6050_ZG_OFFSET_L  = 0x18,
+    MPU6050_SAMPLRT      = 0x19, // Sample Rate Divider
+    MPU6050_CONFIG       = 0x1A, // General config (DLPF etc.)
+    MPU6050_GYRO_CONFIG  = 0x1B, // Gyro config
+    MPU6050_ACCEL_CONFIG = 0x1C, // Accelerometer config
+	MPU6050_ACCEL_CONFIG2 = 0x1D, // Accelerometer config
+
+    MPU6050_PWR_MGMT_1   = 0x6B, // Power management 1
+    MPU6050_PWR_MGMT_2   = 0x6C, // Power management 2
+
+    MPU6050_TEMP_OUT_H   = 0x41, // Temperature high byte
+    MPU6050_GYRO_XOUT_H  = 0x43, // Gyro X high byte
+    MPU6050_ACCEL_XOUT_H = 0x3B, // Accel X high byte
+
+    MPU6050_INT_PIN      = 0x37, // INT pin config
+    MPU6050_INT_ENABLE   = 0x38, // Interrupt enable
+    MPU6050_INT_STATUS   = 0x3A, // Interrupt status
+
+    MPU6050_WAKE_ON_MOTION = 0x1F, // Wake-on-motion
+    MPU6050_FIFO_ENABLE    = 0x23, // FIFO enable
+	MPU6050_FIFO_COUNT      = 0x72, // FIFO read
+    MPU6050_FIFO_READ      = 0x74, // FIFO read
+    MPU6050_USER_CONTROL   = 0x6A  // User control (FIFO, I2C master, reset)
+} MPU6050_RegisterMap;
+
+
+struct mpu6050_t{
+	I2C_HandleTypeDef* i2c_handle;
+	uint8_t i2c_add;
+	uint8_t who_am_i;
+	uint32_t max_delay;
+
+	int8_t accel_xout_h;
+	int8_t accel_xout_l;
+	int8_t accel_yout_h;
+	int8_t accel_yout_l;
+	int8_t accel_zout_h;
+	int8_t accel_zout_l;
+	int8_t temp_out_h;
+	int8_t temp_out_l;
+	int8_t gyro_xout_h;
+	int8_t gyro_xout_l;
+	int8_t gyro_yout_h;
+	int8_t gyro_yout_l;
+	int8_t gyro_zout_h;
+	int8_t gyro_zout_l;
+
+	// Buffer for raw data from sensor
+	uint8_t raw_data[14]; // 6 bytes accel, 2 bytes temp, 6 bytes gyro
+
+	// Final, calculated values in physical units
+	int16_t accel_G[3];     // X, Y, Z in G's
+	int16_t gyro_dps[3];    // X, Y, Z in degrees per second
+	float temperature_C;  // Temperature in Celsius
+
+	float accelIng[3];
+	float gyroIns[3];
+};
+
+MPU6050_Status static write_register(mpu6050_handle_t dev, uint16_t MemAddress, uint8_t *pData, uint16_t Size){
+	if(HAL_I2C_Mem_Write(dev->i2c_handle, dev->i2c_add, MemAddress, 1, pData, Size, dev->max_delay)==HAL_OK){
+		return MPU6050_OK;
 	}
-
-	return REGISTER_READ_UNAVAILABLE;
+	return MPU6050_FAIL;
 }
 
-static MPU6xx0_Status _mpu6xx0readregister_DMA(MPU6xx0_t *dev,
-		uint16_t MemAddress, uint8_t *pData, uint16_t Size) {
-
-	if (HAL_I2C_Mem_Read_DMA(dev->i2c_handle, dev->i2c_add, MemAddress,
-	I2C_MEMADD_SIZE_8BIT, pData, Size) == HAL_OK) {
-		return MPU6xx0_OK;
+MPU6050_Status static read_register(mpu6050_handle_t dev, uint16_t MemAddress, uint8_t *pData, uint16_t Size){
+	if(HAL_I2C_Mem_Read(dev->i2c_handle, dev->i2c_add, MemAddress, 1, pData, Size, dev->max_delay)==HAL_OK){
+		return MPU6050_OK;
 	}
-
-	return REGISTER_READ_UNAVAILABLE;
+	return MPU6050_FAIL;
 }
 
-static MPU6xx0_Status _mpu6xx0writeregister(MPU6xx0_t *dev, uint16_t MemAddress,
-		uint8_t *pData, uint16_t Size) {
-
-	if (HAL_I2C_Mem_Write(dev->i2c_handle, dev->i2c_add, MemAddress,
-	I2C_MEMADD_SIZE_8BIT, pData, Size, HAL_MAX_DELAY) == HAL_OK) {
-		return MPU6xx0_OK;
+MPU6050_Status static read_register_dma(mpu6050_handle_t dev, uint16_t MemAddress, uint8_t *pData, uint16_t Size){
+	if(HAL_I2C_Mem_Read_DMA(dev->i2c_handle, dev->i2c_add, MemAddress, 1, pData, Size)==HAL_OK){
+		return MPU6050_OK;
 	}
-
-	return REGISTER_READ_UNAVAILABLE;
+	return MPU6050_FAIL;
 }
 
-uint8_t MPU6xx0_AutoDetect(MPU6xx0_t *dev) {
-	for (uint8_t add = 0; add < 256; add++) {
-		if (HAL_I2C_IsDeviceReady(dev->i2c_handle, add, 1,
-		HAL_MAX_DELAY) == HAL_OK) {
-			return add;;
-		}
+uint8_t mpu6050_autodetect(I2C_HandleTypeDef* hi2c){
+	if(HAL_I2C_IsDeviceReady(hi2c, i2c_addr_0, 1, 1000)==HAL_OK){
+		return i2c_addr_0;
+	}else if(HAL_I2C_IsDeviceReady(hi2c, i2c_addr_1, 1, 1000)==HAL_OK){
+		return i2c_addr_1;
+	}else{
+		return 0;
 	}
-	if (HAL_I2C_IsDeviceReady(dev->i2c_handle, MPU6xx0_I2C_ADDR_0, 1,
-	HAL_MAX_DELAY) == HAL_OK) {
-		return MPU6xx0_I2C_ADDR_0;
-	} else if (HAL_I2C_IsDeviceReady(dev->i2c_handle, MPU6xx0_I2C_ADDR_1, 1,
-	HAL_MAX_DELAY) == HAL_OK) {
-		return MPU6xx0_I2C_ADDR_1;
-	}
-	return 0;
 }
 
-MPU6xx0_Status MPU6xx0_Init(MPU6xx0_t *dev, uint8_t devadd) {
+mpu6050_handle_t mpu6050_init(mpu6050_config_t* config, uint8_t i2c_address){
+	if(i2c_address!=i2c_addr_0 && i2c_address!=i2c_addr_1) return NULL;
+	mpu6050_handle_t dev = (mpu6050_handle_t)malloc(sizeof(struct mpu6050_t));
+	if(dev==NULL) return NULL;
+	dev->i2c_handle=config->i2c_handle;
+	dev->i2c_add=i2c_address;
+	dev->max_delay=config->max_delay;
 
-	uint8_t databuffer;
-	dev->i2c_add = devadd;
-	if (dev->i2c_add != MPU6xx0_I2C_ADDR_0 && dev->i2c_add != MPU6xx0_I2C_ADDR_1) {
-		databuffer = MPU6xx0_AutoDetect(dev);
-		if (databuffer != 0) {
-			dev->i2c_add = databuffer;
-		} else {
-			return DEVICE_NOT_FOUND;
-		}
+	if(read_register(dev, MPU6050_WHO_AM_I, &dev->who_am_i, 1)!=MPU6050_OK){
+		return NULL;
 	}
-
-	if (_mpu6xx0readregister(dev, MPU6xx0_WHO_AM_I_ADDR, &(dev->who_am_i), 1)
-			!= MPU6xx0_OK) {
-		return REGISTER_READ_UNAVAILABLE;
+	if(dev->who_am_i!=who_am_i_0 && dev->who_am_i!=who_am_i_1){
+		return NULL;
 	}
-
-	if (dev->who_am_i != MPU6050_WHO_AM_I && dev->who_am_i != MPU6500_WHO_AM_I) {
-		return WHO_IS_THIS_DEVICE;
+	uint8_t wake_up_cmd=0;
+	if(write_register(dev, MPU6050_PWR_MGMT_1, &wake_up_cmd, 1)!=MPU6050_OK){
+		return NULL;
 	}
+	HAL_Delay(100); // reset sonrası 100 ms bekleme
+	return dev;
+}
 
-	uint8_t wakeup_cmd = 0x00;
-	if (_mpu6xx0writeregister(dev, MPU6xx0_PWR_MGMT_1, &wakeup_cmd, 1)
-			!= MPU6xx0_OK) {
+
+MPU6050_Status mpu6050_configurate(mpu6050_handle_t dev, mpu6050_config_t* config){
+	if(dev->i2c_add!=i2c_addr_0 && dev->i2c_add!=i2c_addr_1) return MPU6050_FAIL;
+	if(write_register(dev, MPU6050_PWR_MGMT_1, &config->pwr_mgmt1.raw, 1)!=MPU6050_OK){
 		return REGISTER_WRITE_UNAVAILABLE;
 	}
-
-	return MPU6xx0_OK;
+	if(write_register(dev, MPU6050_CONFIG, &config->config.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_GYRO_CONFIG, &config->gyro_config.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_ACCEL_CONFIG, &config->accel_config.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_ACCEL_CONFIG2, &config->accel_config2.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_SAMPLRT, &config->sample_rate, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_INT_PIN, &config->int_pin_config.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_INT_ENABLE, &config->int_en.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_INT_STATUS, &config->int_stat.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_USER_CONTROL, &config->user_ctrl.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	if(write_register(dev, MPU6050_FIFO_ENABLE, &config->fifo_en.raw, 1)!=MPU6050_OK){
+		return REGISTER_WRITE_UNAVAILABLE;
+	}
+	return MPU6050_OK;
 }
 
-MPU6xx0_Status MPU6xx0_Config(MPU6xx0_t *dev, MPU6xx0_Configuration_t *config) {
-
-	if (dev->i2c_handle == NULL) {
-		return UNINITED_DEVICE;
-	}
-
-	uint8_t pwr_mngmnt_buffer[2];
-	uint8_t config_buffer[4];
-	uint8_t int_config_buffer[2];
-	uint8_t fifo_config_buffer;
-
-	pwr_mngmnt_buffer[0] = ((config->Power_Manage1.Device_Reset << 7)
-			| (config->Power_Manage1.Sleep << 6)
-			| (config->Power_Manage1.Cycle << 5)
-			| (config->Power_Manage1.Gyro_Standby << 4)
-			| (config->Power_Manage1.Temp_Dis << 3)
-			| (config->Power_Manage1.ClkSel));
-
-	pwr_mngmnt_buffer[1] = ((config->Power_Manage2.Lp_Wake_Ctrl << 6)
-			| (config->Power_Manage2.Disable_xa << 5)
-			| (config->Power_Manage2.Disable_ya << 4)
-			| (config->Power_Manage2.Disable_za << 3)
-			| (config->Power_Manage2.Disable_xg << 2)
-			| (config->Power_Manage2.Disable_yg << 1)
-			| (config->Power_Manage2.Disable_zg));
-
-	if (_mpu6xx0writeregister(dev, MPU6xx0_PWR_MGMT_1, pwr_mngmnt_buffer, 1)
-			!= MPU6xx0_OK) {
-		return REGISTER_WRITE_UNAVAILABLE;
-	}
-
-	config_buffer[0] = config->Sample_Rate;
-	config_buffer[1] = ((0 << 7) | (config->Config.Fifo_Mode << 6)
-			| (config->Config.Ext_Sync_Set << 3) | (config->Config.Dlpf_Cfg));
-	config_buffer[2] = 0x00
-			| ((config->Gyro_FS_Sel << 3) | (config->Fchoice_B));
-	config_buffer[3] = 0x00
-			| ((config->Accel_Fchoice_B << 3) | (config->A_Dlpf_Cfg));
-
-	if (_mpu6xx0writeregister(dev, MPU6xx0_SAMPLRT_ADDR, config_buffer, 4)
-			!= MPU6xx0_OK) {
-		return REGISTER_WRITE_UNAVAILABLE;
-	}
-
-	int_config_buffer[0] = (config->Int_pin.Actl << 7)
-			| (config->Int_pin.Open << 6) | (config->Int_pin.Latch_Int_En << 5)
-			| (config->Int_pin.Int_Anyrd_2clear << 4)
-			| (config->Int_pin.Actl_Fsync << 3)
-			| (config->Int_pin.Fsync_Int_Mode_En << 2)
-			| (config->Int_pin.Bypass_En << 1);
-	int_config_buffer[1] = (config->Int_Enable.Wom_En << 6)
-			| (config->Int_Enable.Fifo_OverFlow_En << 4)
-			| (config->Int_Enable.Fsync_Int_En << 3)
-			| (config->Int_Enable.RAW_RDY_EN);
-
-	if (_mpu6xx0writeregister(dev, MPU6050_INT_PIN_ADDR, int_config_buffer, 2)
-			!= MPU6xx0_OK) {
-		return REGISTER_WRITE_UNAVAILABLE;
-	}
-
-	fifo_config_buffer = (config->FIFO_Enable.TEMP_OUT << 7)
-			| (config->FIFO_Enable.GYRO_XOUT << 6)
-			| (config->FIFO_Enable.GYRO_YOUT << 5)
-			| (config->FIFO_Enable.GYRO_ZOUT << 4)
-			| (config->FIFO_Enable.ACCEL << 3)
-			| (config->FIFO_Enable.SLV_2 << 2)
-			| (config->FIFO_Enable.SLV_1 << 1) | (config->FIFO_Enable.SLV_0);
-
-	if (_mpu6xx0writeregister(dev, MPU6050_FIFO_ENABLE_ADDR,
-			&fifo_config_buffer, 1) != MPU6xx0_OK) {
-		return REGISTER_WRITE_UNAVAILABLE;
-	}
-
-	fifo_config_buffer=0x00;
-
-	_mpu6xx0writeregister(dev, MPU6050_USER_CONTROL_ADDR, &fifo_config_buffer, 1);
-
-	fifo_config_buffer=0x04;
-
-	_mpu6xx0writeregister(dev, MPU6050_USER_CONTROL_ADDR, &fifo_config_buffer, 1);
-
-	fifo_config_buffer=0x40;
-
-	_mpu6xx0writeregister(dev, MPU6050_USER_CONTROL_ADDR, &fifo_config_buffer, 1);
-
-	return MPU6xx0_OK;
-}
-
-void inline MPU6xx0_getValues(MPU6xx0_t *dev) {
+void inline mpu6050_get_values(mpu6050_handle_t dev){
 	int16_t tempData;
 	dev->accel_G[0] = (int16_t) ((dev->raw_data[0] << 8) | dev->raw_data[1]);
 	dev->accel_G[1] = (int16_t) ((dev->raw_data[2] << 8) | dev->raw_data[3]);
@@ -185,67 +187,56 @@ void inline MPU6xx0_getValues(MPU6xx0_t *dev) {
 	dev->gyro_dps[1] = (int16_t) ((dev->raw_data[8] << 8) | dev->raw_data[9]);
 	dev->gyro_dps[2] = (int16_t) ((dev->raw_data[10] << 8) | dev->raw_data[11]);
 	tempData = (int16_t) ((dev->raw_data[12] << 8) | dev->raw_data[13]);
-	if (dev->who_am_i == MPU6500_WHO_AM_I) {
+	if (dev->who_am_i == who_am_i_1) {
 		dev->temperature_C = (float) tempData / 333.87 + 21.00;
-	} else if (dev->who_am_i == MPU6050_WHO_AM_I) {
+	} else if (dev->who_am_i == who_am_i_0) {
 		dev->temperature_C = (float) tempData / 340.0 + 36.53;
 	}
 }
 
-void inline MPU6xx0_getValuesIngIns(MPU6xx0_t *dev,
-		MPU6xx0_Configuration_t *config) {
+void inline mpu6050_get_values_ing_ins(mpu6050_handle_t dev,
+		mpu6050_config_t *config) {
 
 	dev->accelIng[0] = (float) ((dev->accel_G[0] / 16384.0)
-			/ (config->Accel_FS_Sel + 1));
+			/ (config->accel_config.bits.accel_fs_sel + 1));
 	dev->accelIng[1] = (float) ((dev->accel_G[1] / 16384.0)
-			/ (config->Accel_FS_Sel + 1));
+			/ (config->accel_config.bits.accel_fs_sel + 1));
 	dev->accelIng[2] = (float) ((dev->accel_G[2] / 16384.0)
-			/ (config->Accel_FS_Sel + 1));
+			/ (config->accel_config.bits.accel_fs_sel + 1));
 
 	dev->gyroIns[0] = (float) ((dev->gyro_dps[0] / 131.0)
-			/ (config->Gyro_FS_Sel + 1));
+			/ (config->gyro_config.bits.gyro_fs_sel + 1));
 	dev->gyroIns[1] = (float) ((dev->gyro_dps[1] / 131.0)
-			/ (config->Gyro_FS_Sel + 1));
+			/ (config->gyro_config.bits.gyro_fs_sel + 1));
 	dev->gyroIns[2] = (float) ((dev->gyro_dps[2] / 131.0)
-			/ (config->Gyro_FS_Sel + 1));
+			/ (config->gyro_config.bits.gyro_fs_sel + 1));
 
 }
 
-bool mpu6xx0_ReadInterruptBits(MPU6xx0_t *dev, MPU6xx0_Configuration_t *config) {
-	uint8_t databuffer;
-	if (_mpu6xx0readregister(dev, MPU6050_INT_STATUS_ADDR, &databuffer, 1)
-			!= MPU6xx0_OK) {
-		return false;
+MPU6050_Status mpu6050_read_values_poll(mpu6050_handle_t dev){
+	if(read_register(dev, MPU6050_ACCEL_XOUT_H, dev->raw_data, 14)==MPU6050_OK){
+		return MPU6050_OK;
 	}
-	config->Int_Stats.Wom_INT = databuffer & 0x40;
-	config->Int_Stats.Fifo_OverFlow_INT = databuffer & 0x10;
-	config->Int_Stats.FSYNC_INT = databuffer & 0x08;
-	config->Int_Stats.DMP_INT = databuffer & 0x02;
-	config->Int_Stats.RAW_DATA_RDY_INT = databuffer & 0x01;
-
-	return true;
+	return REGISTER_READ_UNAVAILABLE;
 }
 
-bool mpu6xx0_ReadFIFO(MPU6xx0_t *dev, uint8_t data[]) {
-	if (_mpu6xx0readregister_DMA(dev, MPU6050_FIFO_READ_ADDR, data, sizeof(data))
-			== MPU6xx0_OK) {
-		return true;
+MPU6050_Status mpu6050_read_values_dma(mpu6050_handle_t dev){
+	if(read_register_dma(dev, MPU6050_ACCEL_XOUT_H, dev->raw_data, 14)==MPU6050_OK){
+		return MPU6050_OK;
 	}
-	return false;
+	return REGISTER_READ_UNAVAILABLE;
 }
 
-bool mpu6xx0_ReadValues_DMA(MPU6xx0_t *dev) {
-	if (_mpu6xx0readregister_DMA(dev, MPU6050_REG_ACCEL_XOUT_H, dev->raw_data,
-			14) == MPU6xx0_OK) {
-		return true;
+MPU6050_Status mpu6050_read_fifo_dma(mpu6050_handle_t dev, uint8_t* data_buffer, uint16_t buffer_size){
+	uint8_t fifo_size[2];
+	if(read_register(dev, MPU6050_FIFO_COUNT, fifo_size, 2)!=MPU6050_OK){
+		return REGISTER_READ_UNAVAILABLE;
 	}
-	return false;
-}
-
-bool mpu6xx0_ReadValues_Polling(MPU6xx0_t *dev) {
-	if (_mpu6xx0readregister(dev, MPU6050_REG_ACCEL_XOUT_H, dev->raw_data, 14)
-			== MPU6xx0_OK) {
-		return true;
+	uint16_t data_size = (fifo_size[0]<<8) | fifo_size[1];
+	if(data_size == 0) return FIFO_EMPTY;
+	uint16_t read_size = (buffer_size > data_size) ? data_size : buffer_size;
+	if(read_register_dma(dev, MPU6050_FIFO_READ, data_buffer, read_size)!=MPU6050_OK){
+		return REGISTER_READ_UNAVAILABLE;
 	}
-	return false;
+	return MPU6050_OK;
 }
